15fea72f151dc152dd959da1a22bea252c3aabc3
Frank Marra <frankrmarra@gmail.com>
2023-02-09 15:32:49 -0500

    working on netlify deploy

--- components/About.jsx
+++ components/About.jsx
@@ -4,9 +4,9 @@ export default function About() {
       <div className="about">
         <p>
           WriteASong.Today is here to jump-start your song writing process.
-          Click on 'Get Inspired' and get a key, popular chord progressions, and
-          a tempo to start writing with. You can also click on either key or
-          tempo to change them independently.
+          Click on &apos;Get Inspired&apos; and get a key, popular chord
+          progressions, and a tempo to start writing with. You can also click on
+          either key or tempo to change them independently.
         </p>
         <br />
         <p>
